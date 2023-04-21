0df75a29d80c03bce62c2fd25d776a0d90cdda07
Anton Bogdanov <xawek@yandex.ru>
2023-04-21 18:31:01 +0500

    pep8 fix

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,10 +2,6 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
-    """
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
